ebb542ec91014c0649cd71686b5b70a00ea36715
zncen <1581623769Jijiorg??>
2023-11-03 14:47:29 +0800

    feat(ts): 利用聯集變數記錄待辦事項完成日期

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 let todoId: number;
 todoId = 1;
 console.log("Todo ID: ", todoId);
+
 const todo = "期中考作業 ";
 console.log("Todo Name: ", todo);
+
 const hasFinished = false;
 console.log("Has Finished:", hasFinished);
 
-let finishDate = new Date();
+let finishDate: Date | undefined;
 console.log("Finish Date: ", finishDate);
