6ad6cce3b00614ac7773c57af62a1ba7dc7c071b
zncen <1581623769Jijiorg??>
2023-11-03 14:45:13 +0800

    feat(ts): 利用 undefined 變數宣告待辦事項完成日期

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 let todoId: number;
 todoId = 1;
 console.log("Todo ID: ", todoId);
-
 const todo = "期中考作業 ";
 console.log("Todo Name: ", todo);
 
 const hasFinished = false;
 console.log("Has Finished:", hasFinished);
+
+let finishDate = undefined;
+console.log("Finish Date: ", finishDate);
