dd212de29f4b0c28840c10c20016a8e60f21cdc9
zncen <1581623769Jijiorg??>
2023-11-03 16:36:36 +0800

    feat(ts): 利用函式設定待辦事項已完成

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
 import { Todo } from "./todo";
-
 const todos = [
-    new Todo(1, "期中考作業"),
-    new Todo(2, "期中考作業-2"),
-    new Todo(3, "期中考作業-3"),
-  ];
+  new Todo(1, "期中考作業"),
+  new Todo(2, "期中考作業-2"),
+  new Todo(3, "期中考作業-3"),
+];
 
-  console.log("To Do: ", todos);
+setTaskFinished(todos[0], new Date());
+
+console.log("To Do: ", todos);
 console.table(todos);
+
+function setTaskFinished(todo: Todo, finishDate: Date): void {
+  todo.hasFinished = true;
+  todo.finishDate = finishDate;
+}
