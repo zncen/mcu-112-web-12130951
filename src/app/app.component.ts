4c812042048630be9eae1db5840924bb605ed69a
zncen <1581623769Jijiorg??>
2023-11-17 15:39:38 +0800

    feat(todo): 利用文字插值顥示待辦事項內容

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { TodoComponent } from './todo/todo.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [TodoComponent],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'todos';
-}
+export class AppComponent {}
--- src/assets/code/index.ts
+++ src/assets/code/index.ts
@@ -1,11 +1,12 @@
-import { Todo } from "./todo";
+import { Todo } from './todo';
+
 const todos = [
-  new Todo(1, "期中考作業"),
-  new Todo(2, "期中考作業-2"),
-  new Todo(3, "期中考作業-3"),
+  new Todo(1, '期中考作業'),
+  new Todo(2, '期中考作業-2'),
+  new Todo(3, '期中考作業-3'),
 ];
 
 todos[0].setFinished(new Date());
 
-console.log("To Do: ", todos);
+console.log('To Do: ', todos);
 console.table(todos);
