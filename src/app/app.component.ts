e0690ae3fb84890a346cb60432a17bc230cf7fe8
zncen <1581623769Jijiorg??>
2023-11-24 15:28:50 +0800

    feat(header): 實作待辦事項頁首元件

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
+
+import { HeaderComponent } from './header/header.component';
 import { TodoComponent } from './todo/todo.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [TodoComponent],
+  imports: [HeaderComponent, TodoComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   content = '待辦事項 A';
-
   hasFinished = false;
 }
