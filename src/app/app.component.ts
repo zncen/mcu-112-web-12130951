f4a1fcd9f29f94ccbad8dbbf1faca5a57ac5a6d8
zncen <1581623769Jijiorg??>
2023-12-03 01:57:47 +0800

    feat(todo-list): 建立工作事項清單元件

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,25 +3,13 @@ import { Component } from '@angular/core';
 
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
-import { Todo } from './model/todo';
-import { TodoComponent } from './todo/todo.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, TodoComponent, FooterComponent, JsonPipe],
+  imports: [HeaderComponent, TodoListComponent, FooterComponent, JsonPipe],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  task = new Todo(1, '待辦事項 A');
-
-  onStateChange(state: boolean): void {
-    if (state) {
-      this.task.setFinished(new Date());
-    } else {
-      this.task.finishDate = undefined;
-      this.task.hasFinished = false;
-    }
-  }
-}
+export class AppComponent {}
