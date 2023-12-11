a5287230197d15badee1a77cd00a84badeed7a81
zncen <1581623769Jijiorg??>
2023-12-11 14:29:49 +0800

    feat(task): 實作工作事項新增作業

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { JsonPipe } from '@angular/common';
 import { Component, inject } from '@angular/core';
-
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
 import { Todo } from './model/todo';
 import { TaskService } from './services/task.service';
-
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -19,6 +17,10 @@ export class AppComponent {
 
   tasks = [new Todo(1, '待辦事項 A'), new Todo(2, '待辦事項 B')];
 
+  onAdd(): void {
+    this.taskService.add('待辦事項 C');
+  }
+
   onStateChange(task: { index: number; state: boolean }): void {
     if (task.state) {
       this.tasks[task.index].setFinished(new Date());
@@ -26,11 +28,5 @@ export class AppComponent {
       this.tasks[task.index].finishDate = undefined;
       this.tasks[task.index].hasFinished = false;
     }
-  }
-  onSet(): void {
-    this.tasks = [new Todo(1, '待辦事項 A'), new Todo(2, '待辦事項 B')];
-  }
-  onClear(): void {
-    this.tasks = [];
   }
 }
--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,12 +1,16 @@
 import { Injectable } from '@angular/core';
-
 import { Todo } from '../model/todo';
-
 @Injectable({
   providedIn: 'root',
 })
 export class TaskService {
   tasks: Todo[] = [];
-
-  constructor() {}
+  add(content: string): void {
+    const id =
+      this.tasks.length === 0
+        ? 1
+        : Math.max(...this.tasks.map((task) => task.id)) + 1;
+    const task = new Todo(id, content);
+    this.tasks.push(task);
+  }
 }
