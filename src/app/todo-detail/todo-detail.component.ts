14e218ea842844235c5515c8ada70dd986b01807
zncen <1581623769Jijiorg??>
2023-12-15 15:10:24 +0800

    feat(task-detail): 使用 async pipe 實作待辦事項明細顥示

--- src/app/todo-detail/todo-detail.component.ts
+++ src/app/todo-detail/todo-detail.component.ts
@@ -1,4 +1,4 @@
-import { CommonModule } from '@angular/common';
+import { AsyncPipe, NgIf } from '@angular/common';
 import {
   Component,
   HostBinding,
@@ -7,6 +7,7 @@ import {
   numberAttribute,
   OnChanges,
 } from '@angular/core';
+import { Observable } from 'rxjs';
 
 import { Todo } from '../model/todo';
 import { TaskRemoteService } from '../services/task-remote.service';
@@ -14,15 +15,14 @@ import { TaskRemoteService } from '../services/task-remote.service';
 @Component({
   selector: 'app-todo-detail',
   standalone: true,
-  imports: [CommonModule],
+  imports: [NgIf, AsyncPipe],
   templateUrl: './todo-detail.component.html',
   styleUrl: './todo-detail.component.css',
 })
 export class TodoDetailComponent implements OnChanges {
   @Input({ transform: numberAttribute })
   id!: number;
-
-  task?: Todo;
+  task$!: Observable<Todo | undefined>;
 
   private readonly taskService = inject(TaskRemoteService);
 
@@ -30,6 +30,6 @@ export class TodoDetailComponent implements OnChanges {
   class = 'todo-detail';
 
   ngOnChanges(): void {
-    this.taskService.getById(this.id).subscribe((task) => (this.task = task));
+    this.task$ = this.taskService.getById(this.id);
   }
 }
