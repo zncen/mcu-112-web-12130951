0a576580fb3191f159dbbb60709e159e22f58b7d
zncen <1581623769Jijiorg??>
2023-12-15 15:06:35 +0800

    feat(task-detail): 實作從後端服務取得待辦事項明細資料

--- src/app/services/task-remote.service.ts
+++ src/app/services/task-remote.service.ts
@@ -1,9 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-
 import { Todo } from '../model/todo';
-
 @Injectable({
   providedIn: 'root',
 })
@@ -12,22 +10,19 @@ export class TaskRemoteService {
 
   private readonly httpClient = inject(HttpClient);
 
-  getById(id: number): Todo | undefined {
-    throw new Error('Method not implemented.');
+  getById(id: number): Observable<Todo | undefined> {
+    return this.httpClient.get<Todo | undefined>(`${this.url}/${id}`);
   }
 
   getAll(): Observable<Todo[]> {
     return this.httpClient.get<Todo[]>(this.url);
   }
-
   add(content: string): void {
     throw new Error('Method not implemented.');
   }
-
   updateState(id: number, hasFinished: boolean): void {
     throw new Error('Method not implemented.');
   }
-
   remove(id: number): void {
     throw new Error('Method not implemented.');
   }
--- src/app/todo-detail/todo-detail.component.ts
+++ src/app/todo-detail/todo-detail.component.ts
@@ -1,16 +1,15 @@
+import { CommonModule } from '@angular/common';
 import {
   Component,
-  Input,
-  inject,
-  numberAttribute,
-  OnInit,
   HostBinding,
+  inject,
+  Input,
+  numberAttribute,
   OnChanges,
-  SimpleChanges,
 } from '@angular/core';
-import { CommonModule } from '@angular/common';
+
 import { Todo } from '../model/todo';
-import { TaskService } from '../services/task.service';
+import { TaskRemoteService } from '../services/task-remote.service';
 
 @Component({
   selector: 'app-todo-detail',
@@ -25,12 +24,12 @@ export class TodoDetailComponent implements OnChanges {
 
   task?: Todo;
 
-  private readonly taskService = inject(TaskService);
+  private readonly taskService = inject(TaskRemoteService);
 
   @HostBinding('class')
   class = 'todo-detail';
 
   ngOnChanges(): void {
-    this.task = this.taskService.getById(this.id);
+    this.taskService.getById(this.id).subscribe((task) => (this.task = task));
   }
 }
