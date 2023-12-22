354ed729a97865acd0f1c3420215fd960ad09e06
zncen <1581623769Jijiorg??>
2023-12-22 15:57:22 +0800

    feat(todo): 實作不使查詢按鈕查詢資料

--- src/app/todo-search/todo-search.component.ts
+++ src/app/todo-search/todo-search.component.ts
@@ -1,6 +1,7 @@
 import { JsonPipe } from '@angular/common';
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { debounceTime, distinctUntilChanged } from 'rxjs';
 
 @Component({
   selector: 'app-todo-search',
@@ -9,9 +10,15 @@ import { FormControl, ReactiveFormsModule } from '@angular/forms';
   templateUrl: './todo-search.component.html',
   styleUrl: './todo-search.component.css',
 })
-export class TodoSearchComponent {
+export class TodoSearchComponent implements OnInit {
   @Output()
   readonly search = new EventEmitter<string | null>();
 
   readonly formControl = new FormControl<string | null>(null);
+
+  ngOnInit(): void {
+    this.formControl.valueChanges
+      .pipe(distinctUntilChanged(), debounceTime(500))
+      .subscribe((value) => this.search.emit(value));
+  }
 }
