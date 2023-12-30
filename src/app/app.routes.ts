622b0800404e27faba17eec6dd66408ee816c75c
zncen <1581623769Jijiorg??>
2023-12-30 17:39:53 +0800

    feat(router): 切換登入與註冊頁面實作

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,12 @@
 import { Routes } from '@angular/router';
+
 import { TodoPageComponent } from './todo-page/todo-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { RegisterPageComponent } from './register-page/register-page.component';
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
   { path: 'home', component: TodoPageComponent },
+  { path: 'login', component: LoginPageComponent },
+  { path: 'register', component: RegisterPageComponent },
 ];
