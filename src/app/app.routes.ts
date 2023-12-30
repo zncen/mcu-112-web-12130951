63a4a98003905dce01aa168e60a8428314d9493d
zncen <1581623769Jijiorg??>
2023-12-30 17:45:36 +0800

    feat(404): 找不到路由頁面實作

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,13 @@
 import { Routes } from '@angular/router';
-
 import { TodoPageComponent } from './todo-page/todo-page.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { RegisterPageComponent } from './register-page/register-page.component';
+import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
   { path: 'home', component: TodoPageComponent },
   { path: 'login', component: LoginPageComponent },
   { path: 'register', component: RegisterPageComponent },
+  { path: '**', component: NotFoundPageComponent },
 ];
---
+++ src/app/not-found-page/not-found-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-not-found-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './not-found-page.component.html',
+  styleUrl: './not-found-page.component.css'
+})
+export class NotFoundPageComponent {
+
+}
